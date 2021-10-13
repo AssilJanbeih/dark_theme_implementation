634e50ff05483a32ab5232be0b4df19a62c8cd12
AssilJanbeih <assiljanbeih@gmail.com>
2021-10-13 16:55:17 +0300

    code

--- lib/models/dark_theme_prefrences.dart
+++ lib/models/dark_theme_prefrences.dart
@@ -3,17 +3,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 //This class includes getter and setter for the theme value
 // it awaits the shared prefrences package to get the needful instance.
 class DarkThemePreferences {
-  static const THEME_STATUS = "THEMESTATUS";
+  static const theme_status = "THEMESTATUS";
 
   setDarkTheme(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool(THEME_STATUS, value);
+    prefs.setBool(theme_status, value);
   }
 
   Future<bool> getTheme() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getBool(
-          THEME_STATUS,
+          theme_status,
         ) ??
         false;
   }
